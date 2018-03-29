3de3899233db308219c78e99885a570fb1940940
Jacobanna <pieprzyk.jakub@gmail.com>
2018-03-29 22:24:19 +0200

    Second try - should have Calculator methods now.

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,12 @@
 package com.kodilla;
 
 public class Calculator {
+
+    public double add(double firstArgument, double secondArgument){
+        return firstArgument + secondArgument;
+    }
+
+    public double subtract(double firstArgument, double secondArgument){
+        return firstArgument - secondArgument;
+    }
 }
--- src/main/java/com/kodilla/KodillaCourseCalculatorApplication.java
+++ src/main/java/com/kodilla/KodillaCourseCalculatorApplication.java
@@ -8,5 +8,9 @@ public class KodillaCourseCalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaCourseCalculatorApplication.class, args);
+    }{
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(5.3,7.4));
+        System.out.println(calculator.subtract(8,7.6));
     }
 }
